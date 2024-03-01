f06c85337053b5bd3cdff43268af32923d48f5b8
Meentis <Kittisaknightcore@gmail.com>
2024-03-02 02:45:54 +0700

    first commit

---
+++ lib/authPage.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:login/homePage.dart';
+import 'package:login/loginPage.dart';
+
+class authPage extends StatelessWidget {
+  const authPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Firebase Auth', style: TextStyle(color: Colors.white)),
+        ),
+        actions: [Icon(Icons.help, color: Colors.white)],
+        backgroundColor: Color.fromRGBO(40, 84, 48, 1),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return homePage();
+          } else {
+            return loginPage();
+          }
+        },
+      ),
+    );
+  }
+}
